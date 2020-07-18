5e715306434d3d5ed4c99392cfa73c7f714b1f4f
Marco <marco.costa@aluno.ufop.edu.br>
2020-07-18 17:34:58 -0300

    Removing auth

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ var httpServ = require('http').createServer();
 const mosca = require('mosca');
 var mqttServ = new mosca.Server({});
 
-var authenticate = function (client, username, password, callback) {
-    console.log("user",username);
-    console.log("pswd",password);
-    var authorized = (username === process.env.CLIENT_ID && password.toString() === process.env.CLIENT_SECRET );
-    if (authorized) client.user = username;
-    callback(null, authorized);
-}
-function setup() {
-    mqttServ.authenticate = authenticate;
-}
 
 mqttServ.on('ready', setup)
 mqttServ.attachHttpServer(httpServ);

a6f72d2cea2f75965aa91e4c56e8571e5db9336e
Marco Antônio de Oliveira Costa <marco@yardim.com.br>
2020-12-21 22:20:16 -0300

    Melhoria no log

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
 
-var httpServ = require('http').createServer();
-const mosca = require('mosca');
+var httpServ = require("http").createServer();
+const mosca = require("mosca");
 var mqttServ = new mosca.Server({});
 
 var authenticate = function (client, username, password, callback) {
-    var authorized = (username === process.env.CLIENT_ID && password.toString() === process.env.CLIENT_SECRET );
-    if (authorized) client.user = username;
-    callback(null, authorized);
-}
+  var authorized =
+    username === process.env.CLIENT_ID &&
+    password.toString() === process.env.CLIENT_SECRET;
+  if (authorized) client.user = username;
+  callback(null, authorized);
+};
 function setup() {
-    mqttServ.authenticate = authenticate;
+  mqttServ.authenticate = authenticate;
 }
 
-mqttServ.on('ready', setup)
+mqttServ.on("ready", setup);
 mqttServ.attachHttpServer(httpServ);
 
-httpServ.listen(process.env.PORT || 8080);
+httpServ.listen(process.env.PORT || 8080, function (port) {
+  console.log("websocket server listening on port", port);
+});
